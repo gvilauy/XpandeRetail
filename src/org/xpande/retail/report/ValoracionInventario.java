128a66fc9d6856c979f48e67c567300e53981c32
gabriel <gabrielvila13@gmail.com>
2019-08-25 20:51:19 -0300

    OK
    Signed-off-by: gabriel <gabrielvila13@gmail.com>

--- src/org/xpande/retail/report/ValoracionInventario.java
+++ src/org/xpande/retail/report/ValoracionInventario.java
@@ -4,7 +4,6 @@ import org.adempiere.exceptions.AdempiereException;
 import org.compiere.process.ProcessInfoParameter;
 import org.compiere.process.SvrProcess;
 import org.compiere.util.DB;
-import org.xpande.retail.model.X_Z_LineaProductoSocio;
 import org.xpande.retail.utils.ComercialUtils;
 
 import java.math.BigDecimal;
@@ -13,25 +12,20 @@ import java.sql.ResultSet;
 import java.sql.Timestamp;
 
 /**
- * Reporte de Balance de Servicios.
+ * Reporte de Valoración de Inventario a una determinado fecha para Retail.
  * Product: Adempiere ERP & CRM Smart Business Solution. Localization : Uruguay - Xpande
- * Xpande. Created by Gabriel Vila on 9/11/17.
+ * Xpande. Created by Gabriel Vila on 8/25/19.
  */
-public class BalanceServicios extends SvrProcess {
+public class ValoracionInventario extends SvrProcess {
 
-    private final String TABLA_REPORTE = "Z_RP_Retail_BalServ";
+    private final String TABLA_REPORTE = "Z_RP_RetailValInv";
 
     private int adOrgID = 0;
-    private int cBPartnerID = 0;
     private int zProductoSeccionID = 0;
     private int zProductoRubroID = 0;
     private int zProductoFamiliaID = 0;
     private int zProductoSubfamiliaID = 0;
     private int mProductID = 0;
-    private String upc = "";
-    private int cCurrencyID = 142;
-
-    private Timestamp startDate = null;
     private Timestamp endDate = null;
 
     @Override
@@ -48,9 +42,6 @@ public class BalanceServicios extends SvrProcess {
                     if (name.trim().equalsIgnoreCase("AD_Org_ID")){
                         this.adOrgID = ((BigDecimal)para[i].getParameter()).intValueExact();
                     }
-                    else if (name.trim().equalsIgnoreCase("C_BPartner_ID")){
-                        this.cBPartnerID = ((BigDecimal)para[i].getParameter()).intValueExact();
-                    }
                     else if (name.trim().equalsIgnoreCase("Z_ProductoSeccion_ID")){
                         this.zProductoSeccionID = ((BigDecimal)para[i].getParameter()).intValueExact();
                     }
@@ -66,18 +57,12 @@ public class BalanceServicios extends SvrProcess {
                     else if (name.trim().equalsIgnoreCase("M_Product_ID")){
                         this.mProductID = ((BigDecimal)para[i].getParameter()).intValueExact();
                     }
-                    else if (name.trim().equalsIgnoreCase("UPC")){
-                        this.upc = para[i].getParameter().toString().trim();
-                    }
-
-                    else if (name.trim().equalsIgnoreCase("DateTrx")){
-                        this.startDate = (Timestamp)para[i].getParameter();
-                        this.endDate = (Timestamp)para[i].getParameter_To();
+                    else if (name.trim().equalsIgnoreCase("EndDate")){
+                        this.endDate = (Timestamp)para[i].getParameter();
                     }
                 }
             }
         }
-
     }
 
     @Override
@@ -91,7 +76,6 @@ public class BalanceServicios extends SvrProcess {
 
         return "OK";
     }
-
 
     /***
      * Elimina información anterior para este usuario en tablas de reporte
@@ -115,21 +99,14 @@ public class BalanceServicios extends SvrProcess {
     private void getData() {
 
         String sql = "", action = "";
-
         try{
 
-            // Cadenas de insert en tablas del reporte
-            action = " insert into " + TABLA_REPORTE + " (ad_client_id, ad_org_id, ad_user_id, c_bpartner_id, m_product_id, value, name, datetrx, " +
-                    "z_productoseccion_id, z_productorubro_id, z_productofamilia_id, z_productosubfamilia_id, z_productoupc_id, upc) ";
+           // Cadenas de insert en tablas del reporte
+            action = " insert into " + TABLA_REPORTE + " (ad_client_id, ad_org_id, ad_user_id, m_product_id, codigoproducto, name, enddate, " +
+                    "z_productoseccion_id, z_productorubro_id, z_productofamilia_id, z_productosubfamilia_id) ";
 
             // Armo condicion where dinámica del reporte
             String whereClause = "";
-
-            if (this.cBPartnerID > 0){
-                whereClause += " and p.m_product_id in (select ps.m_product_id from z_productosocio ps where ps.c_bpartner_id =" + this.cBPartnerID +
-                        " and ps.z_productosocio_id in (select z_productosocio_id from z_productosocioorg where ad_orgtrx_id =" + this.adOrgID + ")) ";
-
-            }
 
             if (this.zProductoSeccionID > 0){
                 whereClause += " and p.z_productoseccion_id =" + this.zProductoSeccionID;
@@ -146,20 +123,12 @@ public class BalanceServicios extends SvrProcess {
             if (this.mProductID > 0){
                 whereClause += " and p.m_product_id =" + this.mProductID;
             }
-            if ((this.upc != null) && (!this.upc.trim().equalsIgnoreCase(""))){
-                whereClause += " and pupc.upc ='" + this.upc + "' ";
-            }
 
-
-            sql = " select p.ad_client_id, " + this.adOrgID + ", " + this.getAD_User_ID() + ", "
-                    + ((cBPartnerID > 0) ? String.valueOf(this.cBPartnerID) : "null") + "::numeric(10,0), " +
-                    " p.m_product_id, p.value, p.name, '" +  this.startDate + "', p.z_productoseccion_id, p.z_productorubro_id,  " +
-                    " p.z_productofamilia_id, p.z_productosubfamilia_id, " +
-                    " pupc.z_productoupc_id, pupc.upc " +
+            sql = " select p.ad_client_id, " + this.adOrgID + ", " + this.getAD_User_ID() + ", " +
+                    " p.m_product_id, p.value, p.name, '" +  this.endDate + "', p.z_productoseccion_id, p.z_productorubro_id,  " +
+                    " p.z_productofamilia_id, p.z_productosubfamilia_id " +
                     " from m_product p " +
-                    " left outer join zv_ultimoproductoupc vupc on p.m_product_id = vupc.m_product_id " +
-                    " left outer join z_productoupc pupc on vupc.z_productoupc_id = pupc.z_productoupc_id " +
-                    " where p.issold ='Y' and p.ispurchased ='Y' and p.isactive ='Y' " + whereClause +
+                    " where p.issold ='Y' and p.ispurchased ='Y' and p.isactive ='Y' and p.ProductType ='I' " + whereClause +
                     " order by p.name ";
 
             DB.executeUpdateEx(action + sql, null);
@@ -170,7 +139,6 @@ public class BalanceServicios extends SvrProcess {
         }
 
     }
-
 
     /***
      * Actualiza campos de las Tablas del Reporte.
@@ -186,41 +154,55 @@ public class BalanceServicios extends SvrProcess {
             sql = " select * from " + TABLA_REPORTE + " where ad_user_id =" + this.getAD_User_ID() +
                     " order by m_product_id ";
 
+            pstmt = DB.prepareStatement(sql, get_TrxName());
+            rs = pstmt.executeQuery();
+
+            while(rs.next()){
+
+                // Fecha ultima factura, moneda y precio
+                this.updateInfoFactura(rs.getInt("m_product_id"));
+
+                // Datos costos y PVP
+                this.updateInfoCosto(rs.getInt("m_product_id"));
+            }
+        }
+        catch (Exception e){
+            throw new AdempiereException(e);
+        }
+        finally {
+            DB.close(rs, pstmt);
+            rs = null; pstmt = null;
+        }
+
+    }
+
+    private void updateInfoFactura(int mProductID) {
+
+        String sql = "";
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try{
+            sql = " select dateinvoiced, c_currency_id, priceentered " +
+                    " from zv_historicocompras " +
+                    " where ad_org_id =" + this.adOrgID +
+                    " and m_product_id =" + mProductID +
+                    " and priceentered > 0 " +
+                    " and dateinvoiced <='" + this.endDate + "' " +
+                    " order by dateinvoiced desc ";
+
         	pstmt = DB.prepareStatement(sql, get_TrxName());
         	rs = pstmt.executeQuery();
 
-        	while(rs.next()){
-
-        	    // Precio Promedio Venta
-        	    BigDecimal precioPromedioVta = ComercialUtils.getPrecioPromedioVta(getCtx(), this.getAD_Client_ID(), this.adOrgID,
-                        rs.getInt("m_product_id"), this.cCurrencyID, this.startDate, this.endDate, null);
-
-                // Cantidad comprada
-                BigDecimal cantComprada = ComercialUtils.getCantidadComprada(getCtx(), this.getAD_Client_ID(), this.adOrgID,
-                        rs.getInt("m_product_id"), this.startDate, this.endDate, null);
-
-                // Importe comprado
-                BigDecimal amtComprado = ComercialUtils.getImporteComprado(getCtx(), this.getAD_Client_ID(), this.adOrgID,
-                        rs.getInt("m_product_id"), cCurrencyID, this.startDate, this.endDate, null);
-
-                // Cantidad vendida
-                BigDecimal cantVendida = ComercialUtils.getCantidadVendida(getCtx(), this.getAD_Client_ID(), this.adOrgID,
-                        rs.getInt("m_product_id"), this.startDate, this.endDate, null);
-
-                // Importe vendido
-                BigDecimal amtVendido = ComercialUtils.getImporteVendido(getCtx(), this.getAD_Client_ID(), this.adOrgID,
-                        rs.getInt("m_product_id"), cCurrencyID, this.startDate, this.endDate, null);
+        	if(rs.next()){
 
                 String action = " update " + TABLA_REPORTE +
-                        " set PriceSO =" + precioPromedioVta + ", " +
-                        " qtypurchased =" + cantComprada + ", " +
-                        " amtpurchased =" + amtComprado + ", " +
-                        " qtysold =" + cantVendida + ", " +
-                        " amtsold =" + amtVendido +
+                        " set DateInvoiced ='" + rs.getTimestamp("dateinvoiced") + "', " +
+                        " c_currency_id =" + rs.getInt("c_currency_id") + ", " +
+                        " priceinvoiced =" + rs.getBigDecimal("priceentered") +
                         " where ad_user_id =" + this.getAD_User_ID() +
-                        " and m_product_id =" + rs.getInt("m_product_id");
-
-        	    DB.executeUpdateEx(action, null);
+                        " and m_product_id =" + mProductID;
+                DB.executeUpdateEx(action, null);
         	}
         }
         catch (Exception e){
@@ -230,8 +212,44 @@ public class BalanceServicios extends SvrProcess {
             DB.close(rs, pstmt);
         	rs = null; pstmt = null;
         }
-
     }
 
+    private void updateInfoCosto(int mProductID) {
+
+        String sql = "";
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try{
+            sql = " select c_currency_id, pricefinal, c_currency_id_so, priceso " +
+                    " from Z_HistCostoProd " +
+                    " where ad_orgtrx_id =" + this.adOrgID +
+                    " and m_product_id =" + mProductID +
+                    " and datevalidpo <='" + this.endDate + "' " +
+                    " order by datevalidpo desc ";
+
+            pstmt = DB.prepareStatement(sql, get_TrxName());
+            rs = pstmt.executeQuery();
+
+            if(rs.next()){
+
+                String action = " update " + TABLA_REPORTE +
+                        " set c_currency_1_id =" + rs.getInt("c_currency_id") + ", " +
+                        " pricefinal =" + rs.getBigDecimal("pricefinal") + ", " +
+                        " c_currency_id_so =" + rs.getInt("c_currency_id_so") + ", " +
+                        " priceso =" + rs.getBigDecimal("priceso") +
+                        " where ad_user_id =" + this.getAD_User_ID() +
+                        " and m_product_id =" + mProductID;
+                DB.executeUpdateEx(action, null);
+            }
+        }
+        catch (Exception e){
+            throw new AdempiereException(e);
+        }
+        finally {
+            DB.close(rs, pstmt);
+            rs = null; pstmt = null;
+        }
+    }
 
 }
