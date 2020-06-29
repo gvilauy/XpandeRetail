95e10afa9964f54576857080b1d6de543efe68b9
gabriel <gabrielvila13@gmail.com>
2020-06-29 08:45:23 -0300

    OK

--- src/org/xpande/retail/model/MZGeneraAstoVta.java
+++ src/org/xpande/retail/model/MZGeneraAstoVta.java
@@ -1392,7 +1392,7 @@ public class MZGeneraAstoVta extends X_Z_GeneraAstoVta implements DocAction, Doc
 			if (amtBaseImp == null) amtBaseImp = Env.ZERO;
 
 			this.setAmtAcctDr(amtMediosPago);
-			this.setAmtAcctCr(amtBaseImp.subtract(amtImpuestos));
+			this.setAmtAcctCr(amtBaseImp.add(amtImpuestos));
 			this.setAmtRounding(amtMediosPago.subtract(amtBaseImp).subtract(amtImpuestos));
 			this.saveEx();
 
